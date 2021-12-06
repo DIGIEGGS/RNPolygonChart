ff906cdfd1662dfacc94b4e06432d8900f8a459d
loneddonkey <89466000+loneddonkey@users.noreply.github.com>
2021-12-06 14:09:34 +0300

    Edit package json (#2)
    * package.json edited
    
    * Added index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {Triangle} from './components/Triangle';
+export {Tetragon} from './components/Tetragon';
+export {Pentagon} from './components/Pentagon';
+export {Hexagon} from './components/Hexagon';
